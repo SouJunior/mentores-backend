4b86807f3c2f367ba8293ce61cd806e961c5e3e5
Patrick Otero <patrick.rocha.otero@gmail.com>
2024-01-30 14:41:27 -0300

    refactor: import services on module files

--- src/modules/testimony/testimony.module.ts
+++ src/modules/testimony/testimony.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { GenerateCodeUtil } from '../../shared/utils/generate-code.util';
 import { TestimonyController } from './testimony.controller';
-import { TestimonyService } from './services/deleteTestimony.service';
 import { TestimonyRepository } from './repository/testimony.repository';
 import { MentorRepository } from '../mentors/repository/mentor.repository';
+import { CreateTestimonyService } from './services/createTestimony.service';
+import { DeleteTestimonyService } from './services/deleteTestimony.service';
+import { EditTestimonyService } from './services/editTestimony.service';
+import { GetAllTestimoniesService } from './services/getTestimonies.service';
 
 @Module({
   imports: [ PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [TestimonyController],
-  providers: [TestimonyService, TestimonyRepository, MentorRepository, GenerateCodeUtil],
+  providers: [CreateTestimonyService, DeleteTestimonyService, EditTestimonyService, GetAllTestimoniesService, TestimonyRepository, MentorRepository, GenerateCodeUtil],
   exports: [TestimonyRepository],
 })
 export class TestimonyModule {}
